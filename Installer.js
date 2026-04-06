06ac3125360c98eed87ca1dd400c5fa058b655e7
Airplanesaviation2361 <karan.kuppa@icloud.com>
2026-04-06 12:30:50 +0400

    Fix comments and update script field in Installer.js

---
+++ Installer.js
@@ -0,0 +1,30 @@
+if (!window.gmenu || !window.GMenu) {
+    fetch(
+        "https://raw.githubusercontent.com/tylerbmusic/GeoFS-Addon-Menu/refs/heads/main/addonMenu.js"
+    )
+        .then((response) => response.text())
+        .then((script) => {
+        eval(script);
+    })
+        .then(() => {
+        setTimeout(afterGMenu, 101);
+    });
+} else afterGMenu()
+
+function afterGMenu() {
+	"//  const twLM = new window.GMenu("Geofs Ad Banner Changer v1.2", "twL");"
+}
+
+
+
+
+
+
+	{
+		"name": "Geofs Ad Banner Changer v1.2",
+		"description": "a basic Ad banner changer for geofs",
+		"version": "version number for debugging",
+		"script": "main.js",
+		"author": "AirplanesAviation2361",
+		"repository":  "https://github.com/Geofs-developer-team/Geofs-ad-banner-changer-v1.2.git"
+	},
